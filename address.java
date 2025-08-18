931336c29c43f764070afada72c9b7e8d67a35c3
hariom2005india <srivastavahariom.001@gmail.com>
2025-08-18 15:24:08 +0530

    Create address.java

---
+++ address.java
@@ -0,0 +1,5 @@
+public class AddressSingle {
+    public static void main(String[] args) {
+        System.out.print("My Name: Hariom, House No: 123, City: New Delhi, State: Delhi, Pincode: 110001");
+    }
+}
