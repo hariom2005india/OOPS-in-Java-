e348994bf0a126b3069e2621b5abf0275413a202
hariom2005india <srivastavahariom.001@gmail.com>
2025-08-18 15:27:01 +0530

    Create multiple.java

---
+++ multiple.java
@@ -0,0 +1,9 @@
+public class AddressMulti {
+    public static void main(String[] args) {
+        System.out.println("My Name: Hariom");
+        System.out.println("House No: 123");
+        System.out.println("City: New Delhi");
+        System.out.println("State: Delhi");
+        System.out.println("Pincode: 110001");
+    }
+}
