27af41dafa0df0e9fb1fdfa4e060caadf3931a6b
hariom2005india <srivastavahariom.001@gmail.com>
2025-09-13 11:52:24 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,20 @@
+class Thsdemo{
+  int a,b;
+  public void get(int x , int y){
+   a=x;
+  b=y;
+    
+  }
+  public voidvput(){
+    System.out.println("a"=+a):
+    System.out.println("b"=+b):
+      
+  }
+}
+ class Demo{
+  public static void main (String[]args){
+    thsdemo ob1 =new thsdemo();
+    ob1.get(10,20);
+    ob1.put();
+ }
+ }
