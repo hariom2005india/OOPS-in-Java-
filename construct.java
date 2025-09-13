f0e2a6d0417b86ce097290ef17e2f54cc8f09bc0
hariom2005india <srivastavahariom.001@gmail.com>
2025-09-13 11:41:56 +0530

    Create construct.java

---
+++ construct.java
@@ -0,0 +1,11 @@
+class construct{
+  construct(){
+    super ();
+    System.out.println("Constructer called");
+      
+  }
+ public static void main(String[]args){
+construct geek = new construct();
+   
+ }  
+}
