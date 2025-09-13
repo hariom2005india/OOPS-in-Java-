63ddf6dd12947a0198dde1f507315b97296f92cb
hariom2005india <srivastavahariom.001@gmail.com>
2025-09-13 11:58:47 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -5,15 +5,15 @@ class Thsdemo{
   b=y;
     
   }
-  public voidvput(){
-    System.out.println("a"=+a):
-    System.out.println("b"=+b):
+  public void put(){
+    System.out.println("a="+a);
+    System.out.println("b="+b);
       
   }
 }
- class Demo{
+ class Thsdemp{
   public static void main (String[]args){
-    thsdemo ob1 =new thsdemo();
+    Thsdemo ob1 =new Thsdemo();
     ob1.get(10,20);
     ob1.put();
  }
